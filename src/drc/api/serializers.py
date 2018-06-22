517a795a66afc71aa31399a4ad92a3649fa91ab1
TheophileBlard <theophile.blard@maykinmedia.nl>
2018-06-22 14:34:30 +0200

    updated sphinx documentation

--- src/drc/api/serializers.py
+++ src/drc/api/serializers.py
@@ -1,9 +1,17 @@
+"""
+Serializers of the Document Registratie Component REST API
+"""
+
 from rest_framework import serializers
 
 from drc.datamodel.models import EnkelvoudigInformatieObject
 
 
 class EnkelvoudigInformatieObjectSerializer(serializers.HyperlinkedModelSerializer):
+    """
+    Serializer for the EnkelvoudigInformatieObject model
+    """
+
     class Meta:
         model = EnkelvoudigInformatieObject
         fields = (
